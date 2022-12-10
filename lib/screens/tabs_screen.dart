3e26d64710d814a1da38c0cdb6a09bb9405b0b21
hassan-prog <alielsayed1112@gmail.com>
2022-12-10 05:53:07 +0200

    added popup menu and implemented with minor modifications to the styling

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './select_contact_screen.dart';
 import './calls_screen.dart';
 import './chats_screen.dart';
 import './communities_screen.dart';
 import './status_screen.dart';
+import '../models/contacts_provider.dart';
 
+// ignore: use_key_in_widget_constructors
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({super.key});
-
   @override
   State<TabsScreen> createState() => _TabsScreenState();
 }
@@ -20,23 +22,90 @@ class _TabsScreenState extends State<TabsScreen> {
       initialIndex: 1,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Chat App'),
+          title: const Text(
+            'Chat App',
+            style: TextStyle(fontSize: 24),
+          ),
           bottom: const TabBar(
             tabs: [
               Tab(
                 icon: Icon(Icons.group),
               ),
               Tab(
-                text: 'Chats',
+                child: Text(
+                  'Chats',
+                  style: TextStyle(fontSize: 16),
+                ),
               ),
               Tab(
-                text: 'Status',
+                child: Text(
+                  'Status',
+                  style: TextStyle(fontSize: 16),
+                ),
               ),
               Tab(
-                text: 'Calls',
+                child: Text(
+                  'Calls',
+                  style: TextStyle(fontSize: 16),
+                ),
               ),
             ],
           ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.camera_alt_outlined,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.search_outlined,
+              ),
+              onPressed: () {},
+            ),
+            PopupMenuButton(
+              onSelected: (int selectedValue) {
+                if (selectedValue == 0) {
+                  setState(() {
+                    Navigator.of(context)
+                        .pushNamed(SelectContactScreen.routeName);
+                  });
+                }
+                if (selectedValue == 1) {
+                  setState(() {
+                    Navigator.of(context).pushNamed(StatusScreen.routeName);
+                  });
+                }
+                if (selectedValue == 2) {
+                  setState(() {
+                    Navigator.of(context).pushNamed(CallsScreen.routeName);
+                  });
+                }
+              },
+              icon: const Icon(Icons.more_vert),
+              itemBuilder: (_) => [
+                const PopupMenuItem(
+                  value: 0,
+                  child: Text(
+                    'Select Contacts',
+                  ),
+                ),
+                const PopupMenuItem(
+                  value: 1,
+                  child: Text(
+                    'Status',
+                  ),
+                ),
+                const PopupMenuItem(
+                  value: 2,
+                  child: Text(
+                    'Calls',
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
         body: TabBarView(
           children: [
